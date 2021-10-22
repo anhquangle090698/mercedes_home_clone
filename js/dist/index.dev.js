146e6011b1c4eccf0c0e586f13af768ee97bc8f5
anhquang <leanhquang.ktpm@gmail.com>
2021-10-22 11:51:42 +0700

    'mercedes-home-clone-v1.2-fix-sticker-add-comment-autoprefix-css'

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -1,35 +1,43 @@
 "use strict";
 
+//Button show more product
 var btnNew = document.getElementById("button-new");
 var btnPopular = document.getElementById("button-popular");
-var btnVideos = document.getElementById("button-videos");
+var btnVideos = document.getElementById("button-videos"); //Product hide
+
 var moreNew = document.getElementById("more-new");
 var morePopular = document.getElementById("more-popular");
-var moreVideos = document.getElementById("more-videos");
+var moreVideos = document.getElementById("more-videos"); //Header & navigation
+
 var navbarToggler = document.getElementById("navbar-search__toggle");
 var navbar = document.getElementById("navbar-nav");
-var headerTop = document.getElementById("header-top");
-var sticker__content = document.querySelector(".sticker__content");
-var body = document.getElementById("body");
+var headerTop = document.getElementById("header-top"); //Body element
+
+var body = document.getElementById("body"); //Scroll back to top
+
 var backToTop = document.getElementById("back-to-top");
 $(document).ready(function () {
+  //Handle show more new
   btnNew.onclick = function () {
     moreNew.style.display = "block";
     moreNew.style.paddingTop = 0;
     btnNew.style.display = "none";
-  };
+  }; //Handle show more popular
+
 
   btnPopular.onclick = function () {
     morePopular.style.display = "block";
     morePopular.style.paddingTop = 0;
     btnPopular.style.display = "none";
-  };
+  }; //Handle show more videos
+
 
   btnVideos.onclick = function () {
     moreVideos.style.display = "block";
     moreVideos.style.paddingTop = 0;
     btnVideos.style.display = "none";
-  };
+  }; //Handle show/hide header when scroll
+
 
   window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
@@ -41,13 +49,15 @@ $(document).ready(function () {
     if (currentScrollPos === 0) {
       document.getElementById("header-1").style.transform = "translateY(0px)";
     }
-  };
+  }; //Handle show/hide navigation
+
 
   navbarToggler.addEventListener("click", function () {
     navbar.classList.toggle("navbar-nav--vertical");
     body.classList.toggle("overflow-y");
     headerTop.classList.toggle("header-top--hide");
-  });
+  }); //Handle back to top
+
   backToTop.addEventListener("click", function () {
     window.scrollTo({
       top: 0
--- js/index.js
+++ js/index.js
@@ -1,39 +1,47 @@
+//Button show more product
 const btnNew = document.getElementById("button-new");
 const btnPopular = document.getElementById("button-popular");
 const btnVideos = document.getElementById("button-videos");
 
+//Product hide
 const moreNew = document.getElementById("more-new");
 const morePopular = document.getElementById("more-popular");
 const moreVideos = document.getElementById("more-videos");
 
+//Header & navigation
 const navbarToggler = document.getElementById("navbar-search__toggle");
 const navbar = document.getElementById("navbar-nav");
 const headerTop = document.getElementById("header-top");
 
-const sticker__content = document.querySelector(".sticker__content");
+//Body element
 const body = document.getElementById("body");
 
+//Scroll back to top
 const backToTop = document.getElementById("back-to-top");
 
 $(document).ready(function () {
+  //Handle show more new
   btnNew.onclick = function () {
     moreNew.style.display = "block";
     moreNew.style.paddingTop = 0;
     btnNew.style.display = "none";
   };
 
+  //Handle show more popular
   btnPopular.onclick = function () {
     morePopular.style.display = "block";
     morePopular.style.paddingTop = 0;
     btnPopular.style.display = "none";
   };
 
+  //Handle show more videos
   btnVideos.onclick = function () {
     moreVideos.style.display = "block";
     moreVideos.style.paddingTop = 0;
     btnVideos.style.display = "none";
   };
 
+  //Handle show/hide header when scroll
   window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
     if (currentScrollPos > 120) {
@@ -46,12 +54,14 @@ $(document).ready(function () {
     }
   };
 
+  //Handle show/hide navigation
   navbarToggler.addEventListener("click", () => {
     navbar.classList.toggle("navbar-nav--vertical");
     body.classList.toggle("overflow-y");
     headerTop.classList.toggle("header-top--hide");
   });
 
+  //Handle back to top
   backToTop.addEventListener("click", () => {
     window.scrollTo({
       top: 0,
